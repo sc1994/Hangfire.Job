4e63a39ff6c180991db0158e3a0f3c318eb3b9ab
SunCheng <sunchengsuncheng@gmail.com>
2018-09-15 12:51:34 +0800

    push

--- Hangfire.Job/Startup.cs
+++ Hangfire.Job/Startup.cs
@@ -37,7 +37,6 @@ namespace Hangfire.Job
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseAuthentication();
             app.UseHangfireDashboard();
             app.UseHangfireServer();
 
