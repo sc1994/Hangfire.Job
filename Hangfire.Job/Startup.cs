0fc62dc57af8c92fdc77a9d484a8f3d21603bff4
孙诚 <sunchengsuncheng@gmail.com>
2018-09-17 16:48:17 +0800

    中文

--- Hangfire.Job/Startup.cs
+++ Hangfire.Job/Startup.cs
@@ -39,6 +39,7 @@ namespace Hangfire.Job
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseRequestLocalization("zh");
             app.UseHangfireDashboard("/hangfire", new DashboardOptions()
             {
                 Authorization = new[] { new CustomAuthorizeFilter() }
